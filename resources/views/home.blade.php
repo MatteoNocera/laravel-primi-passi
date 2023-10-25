eb707dd1056d9e25a00097a7cea856032b6ebc4e
MatteoNocera <matteo_nocera@tiscali.it>
2023-10-25 17:43:40 +0200

    try to use css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <!-- My Style -->
+    <link rel="stylesheet" href="/laravel/laravel-primi-passi/resources/css/app.css">
 </head>
 
 <body>
